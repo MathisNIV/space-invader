3fb486dcaa515cfcb45e6eb015f083cd56c84bf9
Rodolphe Lajugie <rodolphe.lajugie@cpe.fr>
2021-12-14 11:22:33 +0100

    block

--- block.py
+++ block.py
@@ -10,9 +10,15 @@ class block():
         self.vie=1
         self.taille_x=1
         self.taille_y=1
-        self.postionx=pX
+        self.position_x=pX
         self.position_y=pY
         
-    def destruction(self):
+    def perdre_vie(self):
         self.vie=0
-        
+        return self.vie
+    
+    def get_position(self):
+        return [self.position_x,self.position_y]
+    
+    def get_taille(self):
+        return [self.taille_x,self.taille_y]
