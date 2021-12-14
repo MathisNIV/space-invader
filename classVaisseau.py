7538c362bd364ceba4f9c87b61761323f421f6f7
Rodolphe Lajugie <rodolphe.lajugie@cpe.fr>
2021-12-14 09:20:03 +0100

    premier pull Rodo

---
+++ classVaisseau.py
@@ -0,0 +1,46 @@
+"""
+auteur : Rodolphe Lajugie
+    
+date de création : 14 décembre 2021
+"""
+    
+class Vaisseau():
+    
+    def __init__(self):
+        self.taille_x = 4
+        self.taille_y = 2
+        self.max_vie = 90
+        self.vie = 90
+        self.position_x = 540
+        self.position_y = 700
+        self.vitesse = 5
+        
+    def get_position(self):
+        "fonction get qui permet d'initialiser le vaisseau en bas et au milieu de la fenêtre"
+        return self.position_x , self.position_y
+    
+    def get_vie(self):
+        return self.vie
+    
+    def deplacer_droite(self):
+        "Permet au vaisseau de se déplacer de sa vitesse si celui-ci n'est pas au bout de la fenêtre"
+        if self.position_x  < 1080:
+            self.position_x += self.vitesse
+        return self.position_x
+    
+    def deplacer_gauche(self):
+        "Permet au vaisseau de se déplacer de sa vitesse si celui-ci n'est pas au début de la fenêtre"
+        if self.position_x > 0:
+            self.position_x -= self.vitesse
+        return self.position_x
+    
+    def perdre_vie(self):
+        """
+        rajouter différents dégats en paramètre si besoin
+        """
+        if self.vie > 0:
+            self.vie -= 30
+        else:
+            print("impossible, mort")
+        return self.vie
+
